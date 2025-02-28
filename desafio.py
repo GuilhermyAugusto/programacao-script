4e263edb863ca1a03b8e529430f24f5ea9ca551c
Guilhermy Augusto Pires Carvalho <guilhermy.carvalho14@gmail.com>
2025-02-27 23:59:40 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,7 +1,9 @@
+#Código para verificar se existem dois números em sequência
 numero= int(input ("Insira um número: "))
-anterior=-1
+anterior=-1 #valores impossíveis
 atual=numero
-calculo=-1
+calculo=-1 #valores impossíveis
+
 while(atual/10!=0):
     anterior=int(atual%10)
     atual=int(atual/10)
